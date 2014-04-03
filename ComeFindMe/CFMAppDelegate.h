454570d0e77dacdd17f54465bd9e0d205b747261
Travis Elnicky <travis.elnicky@gmail.com>
2014-04-03 14:59:10 -0600

    added facebook sdk

---
+++ ComeFindMe/CFMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CFMAppDelegate.h
+//  ComeFindMe
+//
+//  Created by travis elnicky on 4/3/14.
+//  Copyright (c) 2014 Travis Elnicky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CFMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ComeFindMe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ComeFindMe
+//
+//  Created by travis elnicky on 4/3/14.
+//  Copyright (c) 2014 Travis Elnicky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CFMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CFMAppDelegate class]));
+    }
+}
